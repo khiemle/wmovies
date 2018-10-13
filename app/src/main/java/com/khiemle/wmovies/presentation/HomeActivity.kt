696992c2e70f0e9769f830c59ccead1ddf727476
Khiem Le <khiemlq@gmail.com>
2018-10-13 14:41:24 +0700

    [f] change the navigation of home screen

--- app/src/main/java/com/khiemle/wmovies/presentation/HomeActivity.kt
+++ app/src/main/java/com/khiemle/wmovies/presentation/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.khiemle.wmovies.presentation
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import com.khiemle.wmovies.R
+import com.khiemle.wmovies.presentation.screens.MovieDetailsFragment
 import com.khiemle.wmovies.presentation.screens.NowPlayingFragment
 import com.khiemle.wmovies.presentation.screens.TopRatedFragment
 import kotlinx.android.synthetic.main.activity_home.*
@@ -52,4 +54,39 @@ class HomeActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().replace(R.id.frameContent, TopRatedFragment()).commit()
     }
 
+    fun openMovieDetails(id: Long){
+        val fragment = MovieDetailsFragment()
+        val bundle = Bundle()
+        bundle.putLong(MovieDetailsFragment.ARG_MOVIE_ID, id)
+        fragment.arguments = bundle
+        supportFragmentManager.beginTransaction().replace(R.id.frameContent, fragment).addToBackStack(MovieDetailsFragment::class.simpleName).commit()
+    }
+
+    fun showBottomNavigation(showed: Boolean) {
+        when (showed) {
+            true -> {
+                navigation.visibility = View.VISIBLE
+                supportActionBar?.let { it.setDisplayHomeAsUpEnabled(false) }
+            }
+            false -> {
+                navigation.visibility = View.GONE
+                supportActionBar?.let { it.setDisplayHomeAsUpEnabled(true) }
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean{
+        onBackPressed()
+        return true
+    }
+
+    override fun onBackPressed() {
+        val existsFragment = supportFragmentManager.findFragmentById(R.id.frameContent)
+        existsFragment?.let {
+            if (existsFragment is MovieDetailsFragment) {
+                showBottomNavigation(true)
+            }
+        }
+        super.onBackPressed()
+    }
 }
